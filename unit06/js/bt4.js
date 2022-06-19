1dd228994d69a74033bcd2c59c2e3fad07ac5b6e
dodoanquan <dodoanquan1110@gmail.com>
2022-06-19 18:37:46 +0700

    BTVN buoi 6

---
+++ unit06/js/bt4.js
@@ -0,0 +1,45 @@
+const a1 = 12;
+const b1 = -9;
+const a2 = 6;
+const b2 = 6;
+const a3 = -189;
+const b3 = -987;
+const a4 = -78;
+const b4 = 56;
+const a5 = 9.5;
+const b5 = 7.9;
+if (a1 > b1) {
+    console.log(`${a1} la so lon nhat`);
+} else if (a1 < b1) {
+    console.log(`${b1} la so lon nhat`);
+} else{
+    console.log('hai so bang nhau');
+}
+if (a2 > b2) {
+    console.log(`${a2} la so lon nhat`);
+} else if (a2 < b2) {
+    console.log(`${b2} la so lon nhat`);
+} else{
+    console.log('hai so bang nhau');
+}
+if (a3 > b3) {
+    console.log(`${a3} la so lon nhat`);
+} else if (a3 < b3) {
+    console.log(`${b3} la so lon nhat`);
+} else{
+    console.log('hai so bang nhau');
+}
+if (a4 > b4) {
+    console.log(`${a4} la so lon nhat`);
+} else if (a4 < b4) {
+    console.log(`${b4} la so lon nhat`);
+} else{
+    console.log('hai so bang nhau');
+}
+if (a5 > b5) {
+    console.log(`${a5} la so lon nhat`);
+} else if (a5 < b5) {
+    console.log(`${b5} la so lon nhat`);
+} else{
+    console.log('hai so bang nhau');
+}
